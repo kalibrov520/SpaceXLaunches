3c07a0bb0065b1934cb3934b4cfaa0b08ea9f9a7
KIlya <kalibrov520@gmail.com>
2022-06-17 12:19:09 +0200

    iosApp successfully added with shared module

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,16 +1,21 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Ilya Kalibrov on 6/17/22.
+//
+
 import SwiftUI
-import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
-
-	var body: some View {
-		Text(greet)
-	}
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Ilya Kalibrov on 6/17/22.
+//
+
+import SwiftUI
+import shared
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
